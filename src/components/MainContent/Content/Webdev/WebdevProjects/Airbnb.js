f8699ca2a304c7da3789bcfb216ddb95548cbecd
Krzysztof Szafran <54580940+Christopher-Saffron@users.noreply.github.com>
2024-04-25 10:52:13 +0200

    Update Airbnb.js

--- src/components/MainContent/Content/Webdev/WebdevProjects/Airbnb.js
+++ src/components/MainContent/Content/Webdev/WebdevProjects/Airbnb.js
@@ -7,7 +7,7 @@ const Airbnb = () => {
       <div className='projectShow__infoPanel'>
         <p><span className='projectShow__secondColor'>Project Title:</span> Airbnb v2</p>
         <p><span className='projectShow__secondColor'>Tools used:</span> React, Blender, Mongoose, Mongodb Atlas, Paypal, Stripe, Maps, Three.js, Next-js, Tailwind, React-Leaflet,</p>
-        <p><span className='projectShow__secondColor'>Live Link:</span> <a className='projectShow__link' target="_blank" rel="noopener noreferrer" href='https://havirbnb.vercel.app/'>https://havirbnb.vercel.app/</a></p>
+        <p><span className='projectShow__secondColor'>Live Link:</span> <a className='projectShow__link' target="_blank" rel="noopener noreferrer" href='https://nipponair.vercel.app/'>https://havirbnb.vercel.app/</a></p>
         <p><span className='projectShow__secondColor'>Github:</span> <a className='projectShow__link' target="_blank" rel="noopener noreferrer" href='https://github.com/Havir-S/havirbnb'>https://github.com/Havir-S/havirbnb</a></p>
       </div>
       <div className='projectShow__flexBox_img'>
